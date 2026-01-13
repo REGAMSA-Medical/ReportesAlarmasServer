0e6a7d9fe6af4f4db7aed404bdb57989fe790f32
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2026-01-13 13:33:17 -0600

    feature: added user info to the response in signIn and signUp endpoints

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -19,12 +19,12 @@ async def userInfo() -> JSONResponse:
         'role':'Mecatronics Engineer'
     }}, status_code=200)
 
-@router.post("/signUp")
+@router.post('/signUp')
 async def signUp(user_data: UserCreateSerializer, db: Session = Depends(get_db)):
     # Verify if user already exists
     existing_user = db.query(User).filter(User.email == user_data.email).first()
     if existing_user:
-        raise HTTPException(status_code=400, detail="El email ya está registrado")
+        raise HTTPException(status_code=400, detail='El email ya está registrado')
     
     # Hash password and save it on db for the new created user
     hashed_pwd = hash_password(user_data.password)
@@ -34,45 +34,47 @@ async def signUp(user_data: UserCreateSerializer, db: Session = Depends(get_db))
     db.refresh(new_user)
     
     # Generate tokens for direct access after sign In
-    access_token, refresh_token = create_tokens({"sub": new_user.email})
+    access_token, refresh_token = create_tokens({'sub': new_user.email})
     
     return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-        "token_type": "bearer"
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'token_type': 'bearer',
+        'user':new_user
     }
     
-@router.post("/signIn")
+@router.post('/signIn')
 async def signIn(credentials: UserLoginSerializer, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == credentials.email).first()
     
     if not user or not verify_password(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+        raise HTTPException(status_code=401, detail='Credenciales incorrectas')
     
-    access_token, refresh_token = create_tokens({"sub": user.email})
+    access_token, refresh_token = create_tokens({'sub': user.email})
     
     return {
-        "access_token": access_token,
-        "refresh_token": refresh_token,
-        "token_type": "bearer"
+        'access_token': access_token,
+        'refresh_token': refresh_token,
+        'token_type': 'bearer',
+        'user':user
     }
 
-@router.post("/refreshJWT")
+@router.post('/refreshJWT')
 async def refreshJWT(refresh_token: str):
     try:
         payload = jwt.decode(refresh_token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         
         # Validate that token is of refresh type
-        if payload.get("type") != "refresh":
-            raise HTTPException(status_code=401, detail="Token inválido")
+        if payload.get('type') != 'refresh':
+            raise HTTPException(status_code=401, detail='Token inválido')
             
-        email: str = payload.get("sub")
+        email: str = payload.get('sub')
         if email is None:
-            raise HTTPException(status_code=401, detail="Token inválido")
+            raise HTTPException(status_code=401, detail='Token inválido')
             
         # Generate a new tokens pair
-        new_access, new_refresh = create_tokens({"sub": email})
-        return {"access_token": new_access, "refresh_token": new_refresh}
+        new_access, new_refresh = create_tokens({'sub': email})
+        return {'access_token': new_access, 'refresh_token': new_refresh}
         
     except JWTError:
-        raise HTTPException(status_code=401, detail="Token expirado o inválido")
+        raise HTTPException(status_code=401, detail='Token expirado o inválido')
