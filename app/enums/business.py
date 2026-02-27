ee0e0a42a3c37dd74d21c25d8e78220505ed7b86
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2026-02-27 11:37:57 -0600

    wip: Altered name field in Stage model, from varchar to enum

--- app/enums/business.py
+++ app/enums/business.py
@@ -1,10 +1,25 @@
 import enum
 
+"""
+Enums of base business data that must be a rule
+that new business model incoming data must follow
+"""
+
 class OrderStatusEnum(enum.Enum):
     NOT_STARTED = "Not Started"
     IN_PROGRESS = "In Progress"
     COMPLETED = "Completed"
     CANCELED = "Canceled"
+    
+class OrderStageEnum(enum.Enum):
+    ORDER = 'Order'
+    ADMINISTRATION = 'Administration'
+    ENGINEERING = 'Engineering'
+    PRODUCTION = 'Production'
+    TESTING = 'Testing'
+    PACKAGING = 'Packaging'
+    DELIVERY = 'Delivery'
+    RECEIVEMENT = 'Receivement'
     
 class AreaCategoryEnum(enum.Enum):
     ADMINISTRATION = 'Administration'
--- app/models/business.py
+++ app/models/business.py
@@ -1,7 +1,7 @@
 from app.models.base import BaselineModel
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, Enum
 from sqlalchemy.orm import relationship
-from app.enums.business import OrderStatusEnum, AreaCategoryEnum
+from app.enums.business import OrderStatusEnum, AreaCategoryEnum, OrderStageEnum
 
 class Customer(BaselineModel):
     __tablename__ = 'customers'
@@ -17,7 +17,7 @@ class Area(BaselineModel):
     name = Column(String, nullable=False, unique=True)
     managed = Column(Boolean, nullable=False, default=False)
     """ Each Area independently of its name, must have a category. For example, there are 6 types of devices the enterprise produces,
-    then, for every one of these devices, they have their engineering behind, despite they area different, they all are from an
+    then, for every one of these devices, they have their engineering behind, despite their area is different, they all are from an
     area that is of engineering type. So, this is applicable for administration, there are Human Resources and Customer Service,
     these both areas are of administration category"""
     category = Column(Enum(AreaCategoryEnum), nullable=False, default=AreaCategoryEnum.PRODUCTION, index=True) 
@@ -25,7 +25,17 @@ class Area(BaselineModel):
 class Stage(BaselineModel):
     __tablename__ = 'stages'
     
-    name = Column(String, nullable=False, unique=True)
+    """
+    - Pedido: El cliente realiza el pedido
+    - Administración: Se evalua disponibilidad de materiales, personal y tiempo, y negociación con cliente
+    - Ingenieria: Se genera orden de trabajo a ingenieria
+    - Producción: Se produce y ensambla el dispositivo/producto
+    - Testing: Se realizan pruebas y control de calidad
+    - Embalaje: Se empaca el producto para su envio
+    - Envio: Se lleva a centros de envio y paqueteria
+    - Cliente: El cliente recibe el producto
+    """
+    name = Column(Enum(OrderStageEnum), nullable=False, unique=True, index=True)
     
 class Order(BaselineModel):
     __tablename__ = 'orders'
