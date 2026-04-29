9fbb5db083326ff558d815a81e688634eb256ac8
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2026-04-29 17:35:44 -0600

    wip: Added docstring to create_order endpoint

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -2,11 +2,20 @@ from fastapi import APIRouter, Depends
 from fastapi import Request
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
+from app.models.business import Order
 from app.decorators.common import handle_http_exceptions
+from app.enums.business import OrderStageEnum, OrderStatusEnum, AreaEnum
 
 router = APIRouter(prefix='/orders', tags=['Orders'])
 
 @router.post('/createOrder')
 @handle_http_exceptions
 async def create_order(request:Request, db:AsyncSession = Depends(get_db)):
+    """
+    Create an order from scratch, obtaining the data from request.forn.
+    Include a selected customer from an existing list, the stage is ORDER by default,
+    the status is IN_PROGRESS by default (because its created but not assigned (COMPLETED) to production yet),
+    the area_id can be any production area, but ALARMS by default,
+    the description is optional, and free.
+    """
     pass
